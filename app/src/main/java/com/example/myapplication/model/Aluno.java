dea00eaf0f6cb731746d70e6095e4192f2a50266
evandro_gdn <evandrogardolinn@gmail.com>
2020-04-06 23:30:43 -0300

    [ADD] #model - adicionado model para representar o objeto aluno

---
+++ app/src/main/java/com/example/myapplication/model/Aluno.java
@@ -0,0 +1,80 @@
+package com.example.myapplication.model;
+
+public class Aluno {
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private String site;
+    private String foto;
+    private Double nota;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", site='" + site + '\'' +
+                ", foto='" + foto + '\'' +
+                ", nota=" + nota +
+                '}';
+    }
+}
