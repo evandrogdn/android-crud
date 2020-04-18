227ce0ddf494cdd5547a64597efb2450b7179a9f
evandro_gdn <evandrogardolinn@gmail.com>
2020-04-18 15:05:39 -0300

    [ADD] #adapter - Adicionei um helper adapter para conseguir realizar a listagem dos alunos

---
+++ app/src/main/java/com/example/myapplication/helper/AlunoAdapter.java
@@ -0,0 +1,51 @@
+package com.example.myapplication.helper;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.myapplication.R;
+import com.example.myapplication.model.Aluno;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class AlunoAdapter extends ArrayAdapter<Aluno> {
+    public AlunoAdapter(Context context, ArrayList<Aluno> alunos) {
+        super(context, 0, alunos);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Aluno aluno = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item, parent, false);
+        }
+
+        ImageView foto = (ImageView) convertView.findViewById(R.id.foto);
+        TextView nome = (TextView) convertView.findViewById(R.id.nome);
+
+        // tratamento para carregamento da imagem
+        File imagem = new File(aluno.getFoto());
+
+        if (imagem.exists()) {
+            Bitmap image = BitmapFactory.decodeFile(imagem.getAbsolutePath());
+            foto.setImageBitmap(image);
+        }
+
+        nome.setText(aluno.getNome());
+
+        return convertView;
+    }
+}
